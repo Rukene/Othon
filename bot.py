65a47b29732c5f9cde44ed77ff58e14606de3bad
Rukene <gbrukene@gmail.com>
2022-11-17 02:21:02 +0100

    Initial commit

---
+++ bot.py
@@ -0,0 +1,124 @@
+import asyncio
+import logging
+import os
+from typing import Optional, Literal
+
+import discord
+from discord.ext import commands
+from discord import app_commands
+from dotenv import dotenv_values
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s (%(name)s %(module)s) %(message)s",
+)
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+async def main():
+    bot = commands.Bot(
+       command_prefix=commands.when_mentioned,
+        description="General purpose bot (FR)",
+        help_command=None,
+        intents=intents 
+    )
+    bot.config = dotenv_values('.env')
+    
+    async with bot:
+        print("Chargement des modules :")
+        for file in os.listdir(f"./cogs"):
+            if file.endswith(".py"):
+                extension = file[:-3]
+                try:
+                    await bot.load_extension(f"cogs.{extension}")
+                    print(f"- '{extension}'")
+                except Exception as e:
+                    exception = f"{type(e).__name__}: {e}"
+                    print(f"x Erreur {extension}\n{exception}")
+        print('--------------')
+        
+        @bot.event
+        async def on_ready():
+            print(f"> Logged in as {bot.user.name}")
+            print(f"> discord.py API version: {discord.__version__}")
+            print("> Invite : {}".format(discord.utils.oauth_url(int(bot.config["APP_ID"]), permissions=discord.Permissions(int(bot.config['PERMISSIONS_INT'])))))
+            print("-------------------")
+    
+        @bot.tree.error
+        async def on_command_error(interaction: discord.Interaction, error):
+            if isinstance(error, app_commands.errors.CommandOnCooldown):
+                minutes, seconds = divmod(error.retry_after, 60)
+                hours, minutes = divmod(minutes, 60)
+                hours = hours % 24
+                msg = f"**Cooldown ·** Tu pourras réutiliser la commande dans {f'{round(hours)} heures' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} secondes' if round(seconds) > 0 else ''}."
+                return await interaction.response.send_message(content=msg)
+            elif isinstance(error, app_commands.errors.MissingPermissions):
+                msg = f"**Erreur ·** Tu manques des permissions `" + ", ".join(error.missing_permissions) + "` pour cette commande !"
+                return await interaction.response.send_message(content=msg)
+            
+
+        @bot.hybrid_command(name="ping", description="Renvoie un pong")
+        async def ping(ctx: commands.Context) -> None:
+            """Ping"""
+            await ctx.send(f"Pong ! (`{round(bot.latency * 1000)}ms`)")
+
+        @bot.event
+        async def on_message(message):
+            if message.author == bot.user:
+                return
+            if message.content.startswith("C'est l'anniversaire"):
+                print('Message envoyé')
+                await message.channel.send('Jus de pute !')
+            if message.content.startswith("C'EST L'ANNIVERSAIRE"):
+                print('Message envoyé')
+                print('Test Github Push')
+                await message.channel.send('JUS DE PUTE !')
+
+        @bot.command(name='appsync')
+        @commands.guild_only()
+        @commands.is_owner()
+        async def appsync(ctx: commands.Context, guilds: commands.Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+            """Synchronisation des commandes localement ou globalement
+            
+            sync -> global sync
+            sync ~ -> sync current guild
+            sync * -> copies all global app commands to current guild and syncs
+            sync ^ -> clears all commands from the current guild target and syncs (removes guild commands)
+            sync id_1 id_2 -> syncs guilds with id 1 and 2
+            """
+            if not guilds:
+                if spec == "~":
+                    synced = await ctx.bot.tree.sync(guild=ctx.guild)
+                elif spec == "*":
+                    ctx.bot.tree.copy_global_to(guild=ctx.guild)
+                    synced = await ctx.bot.tree.sync(guild=ctx.guild)
+                elif spec == "^":
+                    ctx.bot.tree.clear_commands(guild=ctx.guild)
+                    await ctx.bot.tree.sync(guild=ctx.guild)
+                    synced = []
+                else:
+                    synced = await ctx.bot.tree.sync()
+
+                await ctx.send(
+                    f"Synchronisation de {len(synced)} commandes {'globales' if spec is None else 'au serveur actuel'} effectuée." 
+                )
+                return
+
+            ret = 0
+            for guild in guilds:
+                try:
+                    await ctx.bot.tree.sync(guild=guild)
+                except discord.HTTPException:
+                    pass
+                else:
+                    ret += 1
+
+            await ctx.send(f"Arbre synchronisé dans {ret}/{len(guilds)}.")
+
+            
+        await bot.start(bot.config['TOKEN'])
+            
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ cogs/birthdays.py
@@ -0,0 +1,158 @@
+import discord
+import logging
+import time
+from datetime import datetime
+import operator
+from discord import app_commands
+from discord.app_commands import Choice
+from discord.ext import commands, tasks
+from tinydb import Query
+
+from common.dataio import get_database
+
+logger = logging.getLogger('galba.Birthdays')
+
+bday_group = app_commands.Group(name="bday", description="Gestion des anniversaires")
+
+MONTHS_CHOICES = [
+    Choice(name='Janvier', value=1),
+    Choice(name='Février', value=2),
+    Choice(name='Mars', value=3),
+    Choice(name='Avril', value=4),
+    Choice(name='Mai', value=5),
+    Choice(name='Juin', value=6),
+    Choice(name='Juillet', value=7),
+    Choice(name='Août', value=8),
+    Choice(name='Septembre', value=9),
+    Choice(name='Octobre', value=10),
+    Choice(name='Novembre', value=11),
+    Choice(name='Décembre', value=12)
+]
+
+class Birthdays(commands.GroupCog, group_name="bday", description="Gestion des anniversaires"):
+    """Gestion et traçage des anniversaires"""
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+    #   self.task_bday.start()
+        
+        self.context_menu = app_commands.ContextMenu(
+            name='Anniversaire',
+            callback=self.ctx_usercommand_bday
+        )
+        self.bot.tree.add_command(self.context_menu)
+        
+        
+    # async def cog_unload(self) -> None:
+    #     self.task_bday.cancel()
+            
+    # @tasks.loop(hours=1.0)
+    # async def task_bday(self):
+    #     now_day, now_month = int(time.strftime('%d', time.localtime())), int(time.strftime('%m', time.localtime()))
+    #     logger.info(f"Check Bday effectué - {now_day}/{now_month}")
+    #     # TODO: Ajouter l'attribution auto du rôle configuré
+        
+    # @task_bday.before_loop
+    # async def before_task_bday(self):
+    #     await self.bot.wait_until_ready()
+    #     logger.info("Start task_bday")
+        
+        
+    # # GUILD LEVEL ----------------------------------
+        
+    # @app_commands.command(name='bdayrole')
+    # @app_commands.guild_only()
+    # @app_commands.default_permissions(manage_messages=True)
+    # async def bdayrole(self, interaction: discord.Interaction, role: discord.Role):
+    #     db = get_database('birthdays', str(interaction.guild_id))
+    #     Setting = Query()
+    #     db.upsert({'name': 'role', 'value': role.id}, Setting.name == 'role')
+    #     await interaction.response.send_message(f"Le rôle a bien été configuré sur **{role}** !")
+        
+        
+    # USER LEVEL -----------------------------------
+        
+    @app_commands.command(name="set")
+    @app_commands.choices(mois=MONTHS_CHOICES)
+    async def bday_set(self, interaction: discord.Interaction, jour: app_commands.Range[int, 1, 31], mois: app_commands.Range[int, 1, 12]):
+        """Informer le bot de votre date d'anniversaire (enregistré globalement)
+
+        :param jour: Jour de naissance (1-31)
+        :param mois: Mois de naissance
+        """
+        db = get_database('birthdays')
+        User = Query()
+        db.upsert({'uid': interaction.user.id, 'day': jour, 'month': mois}, User.uid == interaction.user.id)
+        await interaction.response.send_message(f"**Votre anniversaire ({jour}/{mois}) a été enregistré !**\nPour le retirer, utilisez `/bday remove`.")
+        
+    @app_commands.command(name='remove')
+    async def bday_remove(self, interaction: discord.Interaction):
+        """Retirer votre anniversaire de la base de données du bot (global)"""
+        db = get_database('birthdays')
+        User = Query()
+        db.remove(User.uid == interaction.user.id)
+        await interaction.response.send_message(f"Votre anniversaire a été supprimé de la base de données avec succès.")
+        
+    @app_commands.command(name="list")
+    async def bday_list(self, interaction: discord.Interaction):
+        """Consulter les 5 prochains anniversaires sur ce serveur"""
+        guild = interaction.guild
+        await interaction.response.defer()
+        today = datetime.today()
+        members_id = [m.id for m in guild.members]
+        db = get_database('birthdays')
+        User = Query()
+        all_r = db.search(User.uid.test(lambda x: x in members_id))
+        if all_r:
+            annivs = []
+            for r in all_r:
+                user_bday = f"{r['day']}/{r['month']}"
+                user_date = datetime.strptime(user_bday, '%d/%m').replace(year=today.year)
+                if today < user_date:
+                    annivs.append([r['uid'], user_bday, user_date.timestamp(), user_date])
+                else:
+                    annivs.append([r['uid'], user_bday, user_date.replace(year=today.year + 1).timestamp(), user_date.replace(year=today.year + 1)])
+            sorted_r = sorted(annivs, key=operator.itemgetter(2))[:5]
+            if sorted_r:
+                msg = ''
+                for l in sorted_r:
+                    msg += f"• {guild.get_member(l[0]).mention} : `{l[3].strftime('%d/%m/%Y')}`\n"
+                
+                em = discord.Embed(title=f"Prochains anniversaires sur **{guild.name}**", description=msg, color=0x2F3136)
+                em.set_footer(text=f"Anniversaires enregistrés · {len(annivs)}")
+                await interaction.followup.send(embed=em)
+            else:
+                await interaction.followup.send("**Aucun anniversaire n'est à venir ·** Aucun prochain anniversaire n'a été trouvé dans la base de données")
+        else:
+            await interaction.followup.send("**Aucun anniversaire n'est à venir ·** Aucun membre du serveur n'a configuré son anniversaire")
+        
+        
+    async def ctx_usercommand_bday(self, interaction: discord.Interaction, member: discord.Member):
+        """Menu contextuel permettant l'affichage de l'anniversaire du membre visé
+
+        :param user: Utilisateur visé par la commande
+        """
+        today = datetime.today()
+        db = get_database('birthdays')
+        User = Query()
+        r = db.search(User.uid == member.id)
+        if r:
+            user_bday = f"{r[0]['day']}/{r[0]['month']}"
+            userdate = datetime.strptime(user_bday, '%d/%m')
+            userdate = userdate.replace(year=today.year)
+            msg = f"__Anniversaire :__ **{user_bday}**\n"
+
+            if today >= userdate:
+                next_date = userdate.replace(year=today.year + 1)
+            else:
+                next_date = userdate
+            msg += f"__Prochain :__ `{next_date.strftime('%d/%m/%Y')}`"
+        
+            em = discord.Embed(title=f"Anniversaire de **{member.display_name}**", description=msg, color=0x2F3136)
+            em.set_thumbnail(url=member.display_avatar.url)
+            await interaction.response.send_message(embed=em, ephemeral=True)
+        else:
+            await interaction.response.send_message("**Erreur ·** Ce membre n'a pas réglé son anniversaire sur ce bot.", ephemeral=True)
+        
+async def setup(bot):
+    await bot.add_cog(Birthdays(bot))
